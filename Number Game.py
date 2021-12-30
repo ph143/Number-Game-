c447563d5b5d4189af5109b771d2983d1765da57
ph143 <81241272+ph143@users.noreply.github.com>
2021-12-30 21:58:31 +0530

    Add files via upload

---
+++ Number Game.py
@@ -0,0 +1,17 @@
+import time
+import random
+n=random.randint(1,90)
+g=int(input("Guess the number between 1 to 100"))
+while g!=n:
+    if g>n:
+            print("Shorter Number pls")
+    elif g<n:
+            print("Greater Number pls")        
+    g=int(input(""))
+   
+if g==n:
+            print("You're Correct")
+
+            
+
+   
